b146027164699ae42f2801fff83789181a5de1e3
Juan Andrés <jardjys@me.com>
2016-04-12 12:08:39 -0500

    FirstCommit
    Commit1

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BookmindAlfa
+//
+//  Created by Juan Andrés Rocha Díaz on 12/04/16.
+//  Copyright © 2016 Juan Andrés Rocha Díaz. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
