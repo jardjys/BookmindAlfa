ab9016a7cb56aa017f2536143ebc3c74e2602f79
Juan Andrés <jardjys@me.com>
2016-04-12 10:26:06 -0500

    Initial Commit

---
+++ BookmindAlfa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookmindAlfa
+//
+//  Created by Juan Andrés Rocha Díaz on 12/04/16.
+//  Copyright © 2016 Juan Andrés Rocha Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
