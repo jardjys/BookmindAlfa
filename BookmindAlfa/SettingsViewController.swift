58310ce0a640a8b7d96dd43722721b15004a76c6
Juan Andrés <jardjys@hotmail.com>
2016-04-26 23:00:39 -0500

    e
    e

--- BookmindAlfa/SettingsViewController.swift
+++ BookmindAlfa/SettingsViewController.swift
@@ -11,6 +11,7 @@ import Parse
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var acceptButton: UIButton!
     @IBOutlet weak var profPic: UIImageView!
@@ -18,6 +19,7 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var mailText: UITextField!
     @IBOutlet weak var nameText: UITextField!
     var userid = String();
+    var keyboardFlag = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +48,8 @@ class SettingsViewController: UIViewController {
             }
             
         }
+        //Label Style
+        titleLabel.font = UIFont(name: "Montserrat-Bold", size: 20)
         
         //Profile Picture style
         profPic.layer.cornerRadius = profPic.frame.height/1.75
@@ -85,14 +89,112 @@ class SettingsViewController: UIViewController {
         let doneimg = UIImage(named: "Donebutton.png")
         acceptButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFill
         acceptButton.setImage(doneimg, forState: UIControlState.Normal)
+        
+        //Cancel button
+        let cancelimg = UIImage(named: "Cancel.png")
+        cancelButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFill
+        cancelButton.setImage(cancelimg, forState: UIControlState.Normal)
+        
+        //Hide keyboard
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SettingsViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        //Get frame up when keyboard comes this goes in viewdidload
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SettingsViewController.keyboardWillShow(_:)), name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SettingsViewController.keyboardWillHide(_:)), name:UIKeyboardWillHideNotification, object: nil)
 
     }
-
+    
+    //Dismiss Keyboard
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    //Hide and show keyboard up
+    
+    func keyboardWillShow(sender: NSNotification) {
+        if !keyboardFlag {
+            self.view.frame.origin.y -= 150
+            keyboardFlag = true
+        } else {
+            keyboardFlag = true
+        }
+    }
+    
+    func keyboardWillHide(sender: NSNotification) {
+        self.view.frame.origin.y += 150
+        keyboardFlag = false
+    }
+    //Accept pressed
+    @IBAction func acceptPressed(sender: AnyObject) {
+        
+        if ((self.passwordText.text?.isEmpty) == true) {
+            let query2 = PFUser.query()
+            SwiftLoading().showLoading()
+            query2!.getObjectInBackgroundWithId(userid) {
+                (user: PFObject?, error: NSError?) -> Void in
+                if error == nil && user != nil {
+                    user!["name"] = self.nameText.text;
+                    user!["email"] = self.mailText.text;
+                    user?.saveInBackground();
+                } else {
+                    print(error)
+                    SwiftLoading().hideLoading()
+                }
+                
+            }
+        } else {
+            let query2 = PFUser.query()
+            SwiftLoading().showLoading()
+            query2!.getObjectInBackgroundWithId(userid) {
+                (user: PFObject?, error: NSError?) -> Void in
+                if error == nil && user != nil {
+                    user!["name"] = self.nameText.text;
+                    user!["email"] = self.mailText.text;
+                    user!["password"] = self.passwordText.text;
+                    user?.saveInBackground();
+                } else {
+                    print(error)
+                    SwiftLoading().hideLoading()
+                }
+            }
+        }
+      
+        performSegueWithIdentifier("accept", sender: self.acceptButton)
+        
+        
+    }
+    
+    //Cancel pressed
+    @IBAction func cancelPressed(sender: AnyObject) {
+        performSegueWithIdentifier("accept", sender: self.cancelButton)
+    }
+    
+    //Prepare for segue
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let destinationVC : ProfileViewController = segue.destinationViewController as! ProfileViewController
+        destinationVC.iduser = self.userid;
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //Text delegates
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if textField == nameText {
+            mailText.becomeFirstResponder()
+        }
+        if textField == mailText {
+            passwordText.becomeFirstResponder()
+        }
+        if textField == passwordText {
+            mailText.resignFirstResponder()
+        }
+        return true
+    }
 
     /*
     // MARK: - Navigation
